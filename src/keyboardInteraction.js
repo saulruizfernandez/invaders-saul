e378c87b61893dbf4d707aff2bbb91d987c46bb6
saulruizfernandez <saul.ruiz.30@ull.edu.es>
2025-06-03 17:37:29 +0200

    proyectil funciona

--- src/keyboardInteraction.js
+++ src/keyboardInteraction.js
@@ -1,18 +1,11 @@
-import { player } from "./main.js";
-import { Projectile } from "./projectile/projectile.js";
-
-// Array to store active projectiles
-export let projectile_array = [];
+export let keys = {};
 
 addEventListener("keydown", function (e) {
-  if (e.key == "ArrowRight") player.mov_x = player.speed;
-  else if (e.key == "ArrowLeft") player.mov_x = -player.speed;
-  else if (e.key == " ") {
-    projectile_array.push(new Projectile(player.pos_x + 46.5));
+  keys[e.key] = true;
+  if (e.key == " ") {
   }
 });
 
 addEventListener("keyup", function (e) {
-  if (e.key == "ArrowRight" && player.mov_x > 0) player.mov_x = 0;
-  else if (e.key == "ArrowLeft" && player.mov_x < 0) player.mov_x = 0;
+  keys[e.key] = false;
 });
--- src/main.js
+++ src/main.js
@@ -1,18 +1,37 @@
 import { Player } from "./player/player.js";
-import { projectile_array } from "./keyboardInteraction.js";
+import { keys } from "./keyboardInteraction.js";
+import { Projectile } from "./projectile/projectile.js";
 
 const canvas = document.getElementById("game_canvas");
 canvas.width = 1200;
 canvas.height = 800;
 const ctx = canvas.getContext("2d");
+
 export let player = new Player();
 
+let projectile_array = [];
+
+let threshold_projectile = 350;
+
+let old_time = new Date().getTime();
 function update() {
+  if (keys["ArrowRight"]) player.mov_x = player.speed;
+  else if (keys["ArrowLeft"]) player.mov_x = -player.speed;
+  else player.mov_x = 0;
+  if (keys[" "]) {
+    let current_time = new Date().getTime();
+    if (current_time - old_time > threshold_projectile) {
+      projectile_array.push(new Projectile(player.pos_x + 45.5));
+      old_time = current_time;
+    }
+  }
   player.pos_x += player.mov_x;
   if (player.pos_x < 5) player.pos_x = 5;
   if (player.pos_x > 1099) player.pos_x = 1099;
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(player.sprite, player.pos_x, 720, player.width, player.height);
+
   for (let i = projectile_array.length - 1; i >= 0; i--) {
     const element = projectile_array[i];
     if (element.pos_y > 0) {
